185376d4857e7a52012e94c17b67f219acf6546c
Ranjeet Kannaujiya <199953779+ranjeetkannaujiya@users.noreply.github.com>
2026-08-26 13:22:34 +0530

    Fix media serving on Render

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -25,6 +25,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path("", include("home.urls")),
     path("product/", include("products.urls")),
@@ -34,7 +37,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
